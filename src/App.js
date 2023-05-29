eff9ae5963e68b66dcbd015825d75ccb839a4d6c
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-05-29 13:31:23 +0200

    Before changing li's to router Links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // Logic imports
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState, createContext } from "react";
 
 // Pages (component) imports
 import HomePage from "./pages/HomePage";
@@ -9,15 +11,26 @@ import NavBar from "./components/navbar/NavBar";
 // Stylesheet imports
 import "./App.css";
 
+// Context containing user login state
+export const UserContext = createContext(null);
+
 function App() {
+  // State variable containing logged in user
+  const [user, setUser] = useState(null);
+
+  // State variable containing items in cart
+  const [cart, setCart] = useState(null);
+
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/menu" element={<MenuPage />}></Route>
-      </Routes>
-    </Router>
+    <UserContext.Provider value={{ user, setUser, cart, setCart }}>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/menu" element={<MenuPage />}></Route>
+        </Routes>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,14 +1,29 @@
+// Logic imports
 import React from "react";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../App";
 
+// Image imports
+import Logo from "../../images/logos/logo color.png";
+
+// Stylesheet imports
 import "./navbar.css";
-
 function NavBar() {
+  const { user } = useContext(UserContext);
+
   return (
-    <div>
-      <ul>
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Cart</li>
+    <div id="navbar">
+      <img id="navbarLogo" src={Logo} alt="BunDrop logo" />
+      <ul id="linksList">
+        <li className="linkToPage">Home</li>
+        <li className="linkToPage">Menu</li>
+        <li className="linkToPage">Cart</li>
+        {user === null ? (
+          <li className="linkToPage">Login</li>
+        ) : (
+          <li className="linkToPage">Account</li>
+        )}
       </ul>
     </div>
   );
