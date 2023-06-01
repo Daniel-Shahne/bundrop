403be7e0cdfb076514d3ed9212c45437700dda12
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-06-01 11:32:44 +0200

    Login UI finished, functionality not implemented
    Visually its all there. Functionality remains to be implemented.

--- README2.txt
+++ README2.txt
@@ -2,3 +2,6 @@ Setup
 
 1. Launch the menu json server with powershell command: npm run menu-db
     This will run a json-server on port 2000
+
+2. Launch the users json server with powershell command: npm run users-db
+    This will run a json server on port 2001
--- src/pages/loginpage/LoginPage.jsx
+++ src/pages/loginpage/LoginPage.jsx
@@ -1,9 +1,51 @@
+// Logic imports
 import React from "react";
 import { useContext } from "react";
 import { UserContext } from "../../App";
+import { Link } from "react-router-dom";
+
+// image imports
+import Logo from "../../images/logos/logo color.png";
+
+// Stylesheet imports
+import "./loginpage.css";
 
 function LoginPage() {
-  return <div>Login Page</div>;
+  return (
+    <div id="loginPageRoot">
+      <form id="loginContainer">
+        <img src={Logo} alt="BunDrop logo" id="imageRow" />
+        <div className="containerRow inputsRow">
+          <label
+            htmlFor="usernameInput"
+            className="inputLabel"
+            id="usernameLabel"
+          >
+            Username
+          </label>
+          <input type="text" className="inputField" id="usernameInput" />
+        </div>
+        <div className="containerRow inputsRow">
+          <label
+            htmlFor="usernameInput"
+            className="inputLabel"
+            id="passwordLabel"
+          >
+            Password
+          </label>
+          <input type="password" className="inputField" id="passwordInput" />
+        </div>
+        <div className="containerRow buttonsRow">
+          <button className="formButton" type="submit">
+            Login
+          </button>
+          <Link to="/register" className="formButton">
+            Register
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
