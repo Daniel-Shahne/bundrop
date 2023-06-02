198591fe964b5a686bc76a758084514e118baa63
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-06-02 09:32:23 +0200

    Can reach user API
    The attempt login method can reach the json server now and check if credentials match.

--- src/pages/loginpage/loginpagescript.js
+++ src/pages/loginpage/loginpagescript.js
@@ -1,4 +1,5 @@
 // Logic imports
+import { type } from "@testing-library/user-event/dist/type";
 import { json, useNavigate } from "react-router-dom";
 
 export function submitLoginForm(event){
@@ -7,11 +8,9 @@ export function submitLoginForm(event){
     
     event.preventDefault();
     if (document.activeElement.name === "submitLogin"){
-        console.log(`Submitting login with name ${username} and pass ${password}`)
         attemptLogin(username, password)
     }
     else if (document.activeElement.name === "submitRegister"){
-        console.log(`Submitting reister with name ${username} and pass ${password}`)
     }
     else{
         console.error("Unexpected error in submitLoginForm")
@@ -24,8 +23,17 @@ function attemptLogin(username, password){
     fetch("http://localhost:2001/users")
     .then((res) => res.json())
     .then((jsonRes) => {
-        usersJson = jsonRes;
-        console.log(usersJson)
+        usersJson = jsonRes;})
+    .then(() => {
+        const foundUser = usersJson.find(o => o.username === username)
+        if (typeof foundUser !== "undefined"){
+            console.log("User found")
+            if(foundUser.password !== password){
+                console.log("Wrong password")
+            }
+        }
+        else{
+            console.log("found no user")
+        }
     })
-    .then(() => console.log(usersJson))
 }
