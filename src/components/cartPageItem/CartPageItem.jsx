494f721b80c436bf116d0ae7d4b911f2123743f1
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-06-04 18:48:51 +0200

    Barely started working on cartpage
    Added some files to start with but got hungry and left the rest for tomorrow

---
+++ src/components/cartPageItem/CartPageItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CartPageItem({ foodItem }) {
+  return <div></div>;
+}
+
+export default CartPageItem;
--- src/pages/cartpage/CartPage.jsx
+++ src/pages/cartpage/CartPage.jsx
@@ -3,7 +3,14 @@ import { useContext } from "react";
 import { UserContext } from "../../App";
 
 function CartPage() {
-  return <div>Cart page</div>;
+  const { cart } = useContext(UserContext);
+
+  return (
+    <div id="cartPageRoot">
+      <div id="cartSummaryContainer"></div>
+      <div id="cartItemsContainer"></div>
+    </div>
+  );
 }
 
 export default CartPage;
