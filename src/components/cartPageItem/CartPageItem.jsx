ac752dcc012b7d0efd8f80727aeb42bb4b55693a
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-06-05 11:21:28 +0200

    Working on cart page
    Individual cart item in cart page has full functionality, being able to add or remove one of each (might wanna add a button to instantly remove all items). The summary just contains the total price integer as of now.

--- src/components/cartPageItem/CartPageItem.jsx
+++ src/components/cartPageItem/CartPageItem.jsx
@@ -1,8 +1,35 @@
+// Logic imports
 import React from "react";
+import { useContext } from "react";
+import { UserContext } from "../../App";
+import { updateSingleCartItem } from "../../scripts/shoppingCartLogic";
+
+// Stylesheet imports
 import "./cartpageitem.css";
 
 function CartPageItem({ foodItem, foodItemQuantity }) {
-  return <div>Food name: {foodItem.name}</div>;
+  const { cart, setCart } = useContext(UserContext);
+
+  return (
+    <div className="cartPageItemRoot">
+      <span className="itemName">{foodItem.name}</span>
+      <div className="quantityControlContainer">
+        <button
+          className="decrementCartItem cartChangeButton"
+          onClick={() => updateSingleCartItem(cart, setCart, foodItem.id, -1)}
+        >
+          -
+        </button>
+        <span className="itemsInCart">{cart[foodItem.id]}</span>
+        <button
+          className="incrementCartItem cartChangeButton"
+          onClick={() => updateSingleCartItem(cart, setCart, foodItem.id, 1)}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default CartPageItem;
